65ab3a3c79be62472ecef7999900d19af667d751
rhylvin2022 <rhylvin.tinador@hitachi-dps.com>
2024-10-09 00:50:39 +0800

    update more

--- lib/pages/select/select_content.dart
+++ lib/pages/select/select_content.dart
@@ -1,8 +1,10 @@
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
 import 'package:login_statistics/helpers/base_view/base_view.dart';
 import 'package:login_statistics/models/response_data.dart';
 import 'package:login_statistics/widgets/buttons/primary_button.dart';
@@ -22,6 +24,13 @@ class _SelectContentState extends BaseViewState {
   String originalCurl = "";
   String nextPageToken = "";
   String searchTerm = "";
+  String originalStartTime = "";
+  String originalEndTime = "";
+  int daysToExtract = 0;
+  int indexDay = 0;
+  late List<Map<String, String>> data;
+  List<Map<String, DateTime>> dateRanges = [];
+
   @override
   void initState() {
     // TODO: implement initState
@@ -76,12 +85,13 @@ class _SelectContentState extends BaseViewState {
     const int newPageSize = 999;
 
     if (initial) {
+      data = [];
       nextPageToken = '';
       originalCurl = _controller.text;
-      curlCommand = originalCurl;
-    } else {
-      curlCommand = originalCurl;
+      indexDay = 0;
+      daysToExtract = 0;
     }
+    curlCommand = originalCurl;
 
     /// Update pageSize
     if (pageSizeRegex.hasMatch(curlCommand)) {
@@ -100,6 +110,7 @@ class _SelectContentState extends BaseViewState {
     }
 
     originalCurl = curlCommand;
+    print('curl: $originalCurl');
 
     /// Parse the cURL command to extract URL and headers
     Map<String, String> headers = {};
@@ -119,25 +130,99 @@ class _SelectContentState extends BaseViewState {
       headers[match.group(1)!] = match.group(2)!;
     }
 
-    // Extract the query parameters
-    Map<String, String> queryParams =
-        Map<String, String>.from(Uri.parse(url).queryParameters);
-    // print('queryParams[searchTerm]: ${queryParams['searchTerm']}');
-    searchTerm = queryParams['searchTerm'] ?? '';
+    if (initial) {
+      // Extract the query parameters
+      Map<String, String> queryParams =
+          Map<String, String>.from(Uri.parse(url).queryParameters);
+      searchTerm = queryParams['searchTerm'] ?? '';
+      originalStartTime = queryParams['startTime'] ?? '';
+      originalEndTime = queryParams['endTime'] ?? '';
+      print('Original startTime: $originalStartTime');
+      print('Original endTime: $originalEndTime');
 
+      DateTime startOriginalDateTime = DateTime.parse(originalStartTime);
+      DateTime endOriginalDateTime = DateTime.parse(originalEndTime);
+      print('startDateTime: $startOriginalDateTime');
+      print('endDateTime: $endOriginalDateTime');
+
+      Duration duration = endOriginalDateTime.difference(startOriginalDateTime);
+      print('duration: $duration');
+
+      double totalDays = duration.inHours / 24;
+      int roundedDays = totalDays.round();
+      print('roundedDays: $roundedDays');
+      daysToExtract = roundedDays;
+
+      dateRanges.clear();
+      for (int i = 0; i < daysToExtract; i++) {
+        DateTime startDate = startOriginalDateTime.add(Duration(days: i));
+        DateTime endDate = startDate.add(const Duration(days: 1));
+
+        dateRanges.add({
+          'startTime': startDate,
+          'endTime': endDate,
+        });
+      }
+
+      int i = 0;
+      dateRanges.forEach((element) {
+        print('$i $element');
+        i++;
+      });
+
+      headers.forEach((key, value) {
+        print('$key = $value');
+      });
+    }
+
+    ///do API call with updated date range.
+    ///
     try {
-      final response = await http.get(Uri.parse(url), headers: headers);
+      Uri uri = Uri.parse(url);
+      Map<String, String> queryParams = uri.queryParameters;
+      Uri updatedUri = uri.replace(
+        queryParameters: {
+          ...queryParams,
+          'startTime': dateRanges[indexDay]['startTime']!.toIso8601String(),
+          'endTime': dateRanges[indexDay]['endTime']!.toIso8601String(),
+          'pageSize': newPageSize.toString(),
+          'pageToken': nextPageToken,
+        },
+      );
+      print('updatedUri: $updatedUri');
+      // Update the curlCommand with the new startTime and endTime
+      final response = await http.get(updatedUri, headers: headers);
       if (response.statusCode == 200) {
         Map<String, dynamic> jsonMap = jsonDecode(response.body);
-        ResponseData data = ResponseData.fromJson(jsonMap);
+        ResponseData responseData = ResponseData.fromJson(jsonMap);
 
-        data.traces!.forEach((element) {
-          print((element.traceId!.replaceAll(searchTerm, '')));
+        responseData.traces!.forEach((element) {
+          String userID = (element.traceId!.replaceAll(searchTerm, ''));
+          String loginDate =
+              DateFormat('MM/dd').format(dateRanges[indexDay]['endTime']!);
+          print('$loginDate : $userID');
+          data.add({loginDate: userID});
         });
-        print('nextPageToken: ${data.nextPageToken}');
-        if (data.nextPageToken != null) {
-          nextPageToken = data.nextPageToken!;
+        print('nextPageToken: ${responseData.nextPageToken}');
+        if (responseData.nextPageToken != null) {
+          nextPageToken = responseData.nextPageToken!;
           _updateRequest();
+        } else {
+          nextPageToken = '';
+          indexDay++;
+          print('indexDay: $indexDay');
+          if (indexDay == daysToExtract) {
+            print('///////////////////////////DONE///////////////////////////');
+
+            ///done
+            data.forEach((element) {
+              print('${element.keys} : ${element.values}');
+            });
+
+            hideLoadingDialog();
+          } else {
+            _updateRequest();
+          }
         }
       } else {
         print('Request failed with status: ${response.statusCode}');
@@ -158,51 +243,22 @@ class _SelectContentState extends BaseViewState {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          // PrimaryButton(
-          //   buttonText: 'Texas Holdem',
-          //   onPressed: () {
-          //     context.beamToNamed(NavigationRoutes.texasHoldem);
-          //   },
-          // ),
-          // SizedBox(
-          //   height: MediaQuery.of(context).size.height * 0.03,
-          // ),
-          // SecondaryButton(
-          //   buttonText: 'Texas Holdem Generator',
-          //   onPressed: () {
-          //     context.beamToNamed(NavigationRoutes.texasHoldemGenerator);
-          //   },
-          // ),
-          // SizedBox(
-          //   height: MediaQuery.of(context).size.height * 0.06,
-          // ),
-          // PrimaryButton(
-          //   buttonText: 'PLO',
-          //   onPressed: () {
-          //     context.beamToNamed(NavigationRoutes.plo);
-          //   },
-          // ),
-          // SizedBox(
-          //   height: MediaQuery.of(context).size.height * 0.03,
-          // ),
-          // SecondaryButton(
-          //   buttonText: 'PLO Generator',
-          //   onPressed: () {
-          //     context.beamToNamed(NavigationRoutes.ploGenerator);
-          //   },
-          // ),
           TextField(
             keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Enter your curl',
               hintText: 'Paste your curl here',
             ),
             controller: _controller,
           ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.03,
+          ),
           PrimaryButton(
             buttonText: 'Extract',
             onPressed: () {
+              showLoadingDialog();
               _sendRequest(true);
             },
           ),
